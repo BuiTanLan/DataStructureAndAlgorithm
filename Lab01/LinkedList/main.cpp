d36bec9ccc8d6ac5a4984ad1f3d7fd6060d5156c
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2020-12-04 00:05:23 +0700

    linklist

--- Lab01/LinkedList/main.cpp
+++ Lab01/LinkedList/main.cpp
@@ -81,7 +81,8 @@ void removeHead(List *&L)
 	NODE *pNode;
 	pNode = L->pHead;
 	L->pHead = L->pHead->pNext;
-	delete pNode;
+	if (L->pHead == nullptr) L->pTail = nullptr;
+		delete pNode;
 }
 
 
@@ -99,11 +100,129 @@ void removeTail(List *&L)
 	}
 	if (count == 0)
 	{
-		pNode = L->pHead = nullptr;
+		L->pTail = L->pHead = nullptr;
 	}
 	else
 	{
 		qNode->pNext = nullptr;
+		L->pTail = qNode;
 		delete pNode;
 	}
+}
+
+
+// Cau 7 
+void removeAll(List* &L)
+{
+	NODE *pNode;
+	while (L->pHead != nullptr)
+	{
+		pNode = L->pHead;
+		L->pHead = L->pHead->pNext;
+		delete pNode;
+	}
+	L->pTail = L->pHead;
+}
+
+
+// Cau 8:
+void printList(List* L)
+{
+	NODE *pNode;
+	pNode = L->pHead;
+	while (pNode != nullptr)
+	{
+		cout<<pNode->key<<" ";
+		pNode = pNode->pNext;
+	}
+}
+
+
+// Cau 9
+int countElements(List* L)
+{
+	NODE *pNode;
+	int count = 0;
+	pNode = L->pHead;
+	while (pNode != nullptr)
+	{
+		count++;
+		pNode = pNode->pNext;
+	}
+	return count;
+}
+
+
+//Cau 10
+List* reverseList(List* L)
+{
+	List* reverseList = new List(*L);
+	NODE *pCurrent = reverseList->pHead;
+	NODE *reverseTail = reverseList->pHead;
+	NODE *pNext = nullptr;
+	NODE *pPrev = nullptr;
+	while (pCurrent != nullptr)
+	{
+		pNext = pCurrent->pNext;
+		pCurrent->pNext = pPrev;
+		pPrev = pCurrent;
+		pCurrent = pNext;
+	}
+	reverseList->pHead = pPrev;
+	reverseList->pTail = reverseTail;
+	return reverseList;
+}
+
+// Cau 11
+void removeDuplicate(List *&L)
+{
+	NODE *pNode, *qNode, *pDuplicate;
+	pNode = L->pHead;
+
+	while (pNode != nullptr &&  pNode->pNext != nullptr)
+	{
+		qNode = pNode;
+		while(qNode != nullptr)
+		{
+			if(pNode->key == qNode->pNext->key)
+			{
+				pDuplicate = qNode->pNext;
+				qNode->pNext = qNode->pNext->pNext;
+				delete (pDuplicate);				
+			}
+			else
+			{
+				qNode = pNode->pNext;
+			}
+		}
+
+		pNode = pNode->pNext;
+	}
+}
+
+
+// Cau 12
+bool removeElement(List *&L, int key)
+{
+	bool isExist = false;
+	NODE *pNode = L->pHead;
+	NODE *pPrev = nullptr;
+
+	if(pNode != nullptr && pNode->key == key)
+	{
+		removeHead(L);
+		return true;
+	}
+	while( pNode != nullptr && pNode->key != key)
+	{
+		pPrev = pNode;
+		pNode = pNode->pNext;
+	}
+	if(pNode == nullptr) return false;
+	if(pNode->key == key)
+	{
+		pPrev->pNext = pNode->pNext;
+		return true;
+	}
+	return false;
 }
